2bae1a31e4ef6bd0f1071372fe5e69807fad48f2
KerteszJanos <k.janos.jani@gmail.com>
2025-02-11 20:00:41 +0100

    Set up ui

---
+++ Szoftver/ui/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h1>{{ title }}</h1>
+      <button @click="incrementCounter">Click Me</button>
+      <p>Counter: {{ counter }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        title: "Hello Vue!",
+        counter: 0
+      };
+    },
+    methods: {
+      incrementCounter() {
+        this.counter++;
+      }
+    },
+    computed: {
+      doubleCounter() {
+        return this.counter * 2;
+      }
+    },
+    watch: {
+      counter(newValue) {
+        console.log("Counter changed:", newValue);
+      }
+    },
+    mounted() {
+      console.log("Component mounted!");
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: blue;
+  }
+  </style>
